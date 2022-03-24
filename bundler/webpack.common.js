0c2a03c81bf599ca02e71f032db84b599b110c65
Stan2hssn <86244342+Stan2hssn@users.noreply.github.com>
2022-03-24 12:28:24 +0100

    First

---
+++ bundler/webpack.common.js
@@ -0,0 +1,123 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        pageOne: './index.js',
+    },
+    output: {
+        filename: '[name].[fullhash].js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'static', to: ''},
+                {from: '*.json', to: 'assets/fonts', context: "src/fonts", noErrorOnMissing: true},
+                {from: 'node_modules/three/examples/js/libs/draco/', to: './draco'}
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Giro Template',
+            favicon: path.resolve(__dirname, '../static/favicon.ico'),
+            template: path.resolve(__dirname, '../index.html'),
+            filename: "index.html",
+            chunk: ['pageOne'],
+            minify: true,
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(jpg|png|gif|svg|jpeg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {outputPath: 'assets/images/'},
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 10000,
+                            mimetype: 'application/octet-stream',
+                            name: 'Manrope.woff2',
+                            outputPath: 'assets/fonts/'
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(html)$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {sources: false}
+                    }
+                ],
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(fbx|glb|obj|3ds|gltf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {outputPath: 'assets/models/'},
+                    },
+                ],
+            },
+            {
+                test: /\.(bin)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {outputPath: 'assets/models/'},
+                    },
+                ],
+            },
+            {
+                test: /\.(mp3|wav)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {outputPath: 'assets/audios/'}
+                    },
+                ]
+            },
+            {
+                test: /\.(mp4|webm)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {outputPath: 'assets/videos/'}
+                    },
+                ]
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
+                ]
+            },
+            {
+                test: /\.(md)$/,
+                exclude: /node_modules/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ],
+    },
+}
---
+++ index.js
@@ -0,0 +1,96 @@
+import * as THREE from 'three';
+import {GLTFLoader,} from "three/examples/jsm/loaders/GLTFLoader";
+import model from "./src/models/bricks_four.gltf";
+
+
+console.log('work')
+
+const scene = new THREE.Scene();
+
+const size = innerWidth / innerHeight;
+
+let camera = new THREE.PerspectiveCamera(20, size, 0.01, 1000);
+/*const helper = new THREE.CameraHelper(camera);
+scene.add(helper);*/
+camera.position.set(14, 10, 13);
+camera.rotation.set(-0.6556956262415362, 0.7065774856978788, 0.4631763928482476);
+
+
+const canvas = document.querySelector('canvas');
+const renderer = new THREE.WebGLRenderer({
+    antialias: false,
+    canvas: canvas,
+    alpha: true,
+    powerPreference: 'high-performance',
+});
+renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+renderer.setSize(innerWidth, innerHeight);
+renderer.setClearColor(0xffffff, 0);
+renderer.shadowMap.enabled = true;
+
+
+console.log(camera.rotation)
+
+
+window.addEventListener('resize', () => {
+    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = size;
+    camera.updateProjectionMatrix();
+})
+
+
+const axesHELPER = new THREE.AxesHelper(10);
+const clock = new THREE.Clock();
+
+let mixer;
+let loader = new GLTFLoader();
+loader.load(// resource URL
+    model, // called when the resource is loaded
+    function (gltf) {
+        console.log(gltf);
+
+        gltf.scene.children[1].receiveShadow = true;
+        gltf.scene.children[1].castShadow = true;
+
+        let light = new THREE.RectAreaLight(gltf.scene);
+        light.position.set(0, 2, 6);
+        scene.add(light)
+
+
+        mixer = new THREE.AnimationMixer(gltf.scene);
+        const action = mixer.clipAction(gltf.animations[0]);
+
+        setTimeout(function () {
+
+            action.play();
+        }, 500)
+        scene.add(gltf.scene);
+
+        animate();
+
+        function animate() {
+            requestAnimationFrame(animate);
+            const delta = clock.getDelta();
+            mixer.update(delta);
+            renderer.render(scene, camera);
+        }
+
+    }, // called while loading is progressing
+    function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+    }, // called when loading has errors
+    function (error) {
+
+        console.log('An error happened');
+
+    },);
+
+
+/*scene.add(axesHELPER);*/
+
+
+
+
+
